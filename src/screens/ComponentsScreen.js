97066031ab33f76957cc0cb601fa049153995dc5
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-08-06 11:58:21 -0500

    added list screen component

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, StyleSheet, View} from 'react-native'
+
+const ComponentsScreen = () => {
+    return (
+        <View>
+            <Text>Getting started with React Native!</Text>
+            <Text>My name is Izaac</Text>
+        </View>
+    )
+}
+
+export default ComponentsScreen
